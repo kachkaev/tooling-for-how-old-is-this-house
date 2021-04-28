fe84d8af87d94cdd40efcb3ea82e1253a97ea704
Alexander Kachkaev <alexander@kachkaev.ru>
2021-04-28 09:44:08 +0100

    Remove old address parsing logic

--- src/shared/addressesViaRegexps/index.ts
+++
@@ -1,9 +0,0 @@
-export { normalizeBuilding } from "./normalizeBuilding";
-export { normalizePlace } from "./normalizePlace";
-export { normalizeStreet } from "./normalizeStreet";
-
-export {
-  splitAddress,
-  normalizeAddressPart,
-  combineAddressParts,
-} from "./helpersForNormalizing";
