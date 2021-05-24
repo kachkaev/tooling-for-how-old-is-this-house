31617b8ecdccf6af643cc6b35de6d6c43f3fcdd2
Ivan Shumilin <ivanshumilin@gmail.com>
2021-05-24 17:52:31 +0300

    Move types into upload command

--- src/commands/5-prepareUpload.ts
+++ src/commands/5-prepareUpload.ts
@@ -7,13 +7,28 @@ import sortKeys from "sort-keys";
 
 import { deepClean } from "../shared/deepClean";
 import { writeFormattedJson } from "../shared/helpersForJson";
+import { OutputGeometry } from "../shared/outputLayers";
 import {
   getMixedPropertyVariantsFileName,
   getUploadFileName,
   MixedPropertyVariantsFeatureCollection,
-  UploadFeature,
-  UploadFeatureProperties,
-} from "../shared/output";
+} from "../shared/outputMixing";
+
+type UploadFeatureProperties = {
+  fid: number;
+  r_adress?: string;
+  r_architec?: string;
+  r_copyrigh?: string;
+  r_name?: string;
+  r_photo_ur?: string;
+  r_style?: string;
+  r_url?: string;
+  r_wikipedi?: string;
+  r_year_int?: number;
+  r_years_st?: string;
+};
+
+type UploadFeature = turf.Feature<OutputGeometry, UploadFeatureProperties>;
 
 export const prepareUpload: Command = async ({ logger }) => {
   logger.log(chalk.bold("Prepare upload file"));
@@ -34,6 +49,7 @@ export const prepareUpload: Command = async ({ logger }) => {
       r_adress: inputFeature.properties.address,
       r_year_int: inputFeature.properties.derivedCompletionYear,
       r_years_st: inputFeature.properties.completionDates,
+      r_name: inputFeature.properties.name,
     };
     outputFeatures.push(
       turf.feature(
