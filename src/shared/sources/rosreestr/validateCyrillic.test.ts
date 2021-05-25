1860ebf11c7b7264be123d328bd82230a138d05c
Alexander Kachkaev <alexander@kachkaev.ru>
2021-05-25 09:23:14 +0100

    Improve code style in it.each tests

--- src/shared/sources/rosreestr/validateCyrillic.test.ts
+++ src/shared/sources/rosreestr/validateCyrillic.test.ts
@@ -1,15 +1,16 @@
 import { validateCyrillic } from "./validateCyrillic";
 
 describe("checkIfIsReadableCyrillic", () => {
-  it.each([
-    [true, "41"],
-    [false, "РўСЂ"],
-    [false, "РўСЂР°РЅСЃС„РѕСЂРјР°С‚РѕСЂРЅР°СЏ РїРѕРґСЃС‚Р°РЅС†РёСЏ в„– 109"],
-    [true, "улица Такая-то"],
-    [true, "УЛИЦА ТЕСТОВАЯ 10"],
-    [true, 'ГСК "Заря-1"'],
-    [true, ""],
-  ])("returns %p for %p", (expectedResult, input) => {
+  it.each`
+    input                                                              | expectedResult
+    ${"41"}                                                            | ${true}
+    ${"РўСЂ"}                                                          | ${false}
+    ${"РўСЂР°РЅСЃС„РѕСЂРјР°С‚РѕСЂРЅР°СЏ РїРѕРґСЃС‚Р°РЅС†РёСЏ в„– 109"} | ${false}
+    ${"улица Такая-то"}                                                | ${true}
+    ${"УЛИЦА ТЕСТОВАЯ 10"}                                             | ${true}
+    ${'ГСК "Заря-1"'}                                                  | ${true}
+    ${""}                                                              | ${true}
+  `("returns $expectedResult for $input", ({ input, expectedResult }) => {
     expect(validateCyrillic(input)).toEqual(expectedResult);
   });
 });
