b214dc3ec403154966ff64c783e2e6385abf3b7a
Alexander Kachkaev <alexander@kachkaev.ru>
2021-07-10 10:21:02 +0100

    Add temp fix to ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,35 @@ module.exports = {
   extends: ["@kachkaev/eslint-config-react"],
   rules: {
     "import/no-default-export": "error",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md
+      {
+        selector: "class",
+        format: ["StrictPascalCase"],
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+      {
+        selector: ["variableLike"],
+        format: ["strictCamelCase", "StrictPascalCase"],
+        filter: {
+          regex: "^(_|.*[XYZ][A-Z].*)$",
+          match: false,
+        },
+      },
+      {
+        selector: ["memberLike"],
+        leadingUnderscore: "allow",
+        filter: {
+          regex: "^(__ANT_.*|_|.*[XYZ][A-Z].*|__html|.*(--|__).*)$",
+          match: false,
+        },
+        format: ["strictCamelCase", "StrictPascalCase", "UPPER_CASE"],
+      },
+    ],
   },
   overrides: [
     {
