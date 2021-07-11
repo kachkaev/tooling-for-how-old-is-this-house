93a1c563eba89743bc2c801602174f4f448debba
Alexander Kachkaev <alexander@kachkaev.ru>
2021-07-11 11:25:37 +0100

    Fix dev server file watching

--- src/commands/launchNextJsDevServer.ts
+++ src/commands/launchNextJsDevServer.ts
@@ -1,7 +1,7 @@
 import { autoStartCommandIfNeeded, Command } from "@kachkaev/commands";
 import execa from "execa";
 
-import { getMixedOutputLayersFilePath } from "../shared/outputMixing";
+import { getMixedPropertyVariantsFilePath } from "../shared/outputMixing";
 import { getOsmDirPath } from "../shared/sources/osm";
 import { getTerritoryExtentFilePath } from "../shared/territory";
 
@@ -11,7 +11,7 @@ export const launchNextJsDevServer: Command = async () => {
     [
       // Justified by src/pages/poster.tsx
       getOsmDirPath(),
-      getMixedOutputLayersFilePath(),
+      getMixedPropertyVariantsFilePath(),
       getTerritoryExtentFilePath(),
     ],
     {
