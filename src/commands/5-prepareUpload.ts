09a0768fa26fdb5a4e8fc1f4017c58219fb25544
Alexander Kachkaev <alexander@kachkaev.ru>
2021-07-20 22:54:20 +0100

    Add r_copyrights

--- src/commands/5-prepareUpload.ts
+++ src/commands/5-prepareUpload.ts
@@ -10,6 +10,7 @@ import { OutputGeometry } from "../shared/outputLayers";
 import {
   getMixedPropertyVariantsFilePath,
   getUploadFilePath,
+  MixedPropertyVariants,
   MixedPropertyVariantsFeatureCollection,
 } from "../shared/outputMixing";
 import { getTerritoryAddressHandlingConfig } from "../shared/territory";
@@ -21,7 +22,7 @@ interface UploadFeatureProperties {
   /** Using ‘adress’ instead of ‘address’ for consistency with first uploaded cities */
   r_adress?: string;
   r_architect?: string;
-  r_copyright?: string;
+  r_copyrights?: string;
   r_floors?: number;
   r_name?: string;
   r_photo_url?: string;
@@ -32,6 +33,31 @@ interface UploadFeatureProperties {
   r_years_str?: string;
   /* eslint-enable @typescript-eslint/naming-convention */
 }
+
+const generateCopyrights = ({
+  photoUrl,
+  photoSource,
+  photoAuthorName,
+}: MixedPropertyVariants): string | undefined => {
+  if (!photoSource || !photoUrl) {
+    return undefined;
+  }
+
+  if (photoUrl?.startsWith("https://commons.wikimedia.org")) {
+    return `фото: Викимедя Коммонс`;
+  }
+
+  switch (photoSource) {
+    case "mkrf":
+      return `фото: Министерство культуры РФ`;
+    case "wikimapia":
+      return photoAuthorName && photoAuthorName !== "Guest"
+        ? `фото: Викимапия (участник ${photoAuthorName})`
+        : `фото: Викимапия`;
+  }
+
+  return `фото: ${photoSource}`;
+};
 
 type UploadFeature = turf.Feature<OutputGeometry, UploadFeatureProperties>;
 
@@ -73,7 +99,7 @@ export const prepareUpload: Command = async ({ logger }) => {
   const outputFeatures: UploadFeature[] = [];
   for (const inputFeature of inputFeatureCollection.features) {
     const outputFeatureProperties: UploadFeatureProperties = {
-      fid: outputFeatures.length + 1,
+      fid: 0,
 
       /* eslint-disable @typescript-eslint/naming-convention */
       r_adress: removeDefaultRegionFromAddress(
@@ -81,6 +107,7 @@ export const prepareUpload: Command = async ({ logger }) => {
           inputFeature.properties.address,
       ),
       r_architect: inputFeature.properties.architect,
+      r_copyrights: generateCopyrights(inputFeature.properties),
       r_floors: inputFeature.properties.floorCountAboveGround,
       r_name: inputFeature.properties.derivedBeautifiedName,
       r_photo_url: inputFeature.properties.photoUrl,
