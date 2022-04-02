6bfdc02d3c461584d188ca113956d41446ca8529
Alexander Kachkaev <alexander@kachkaev.ru>
2022-04-02 12:53:22 +0100

    Re-enable `import/no-unresolved` ESLint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,6 @@ module.exports = {
     "@kachkaev/eslint-config-react/extra-type-checking",
     "plugin:@next/next/recommended",
   ],
-  rules: {
-    "import/no-unresolved": "off", // https://github.com/import-js/eslint-plugin-import/issues/2132
-  },
   overrides: [
     {
       files: ["src/commands/**"],
