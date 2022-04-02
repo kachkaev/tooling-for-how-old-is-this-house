3b41bbf6a4af1a68e1d26f53b9c54a8d707bdaf5
Alexander Kachkaev <alexander@kachkaev.ru>
2022-04-02 17:20:57 +0100

    Suport old `src/scripts` paths with a CLI warning

---
+++ shared/exe.cjs
@@ -0,0 +1,9 @@
+/** @todo after 2022-06-01
+ * - Delete handle-deprecated-script-in-src.cjs
+ * - Delete exe.cjs
+ * - Check if suppress-experimental-warnings.cjs is still needed
+ * - Update package.json → scripts → exe
+ */
+
+require("./exe/handle-deprecated-script-in-src.cjs");
+require("./exe/suppress-experimental-warnings.cjs");
