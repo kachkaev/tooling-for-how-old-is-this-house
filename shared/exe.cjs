7fd41de2e01f0bad42a9d4da4c391de8b4ddf5f8
Alexander Kachkaev <alexander@kachkaev.ru>
2023-01-10 22:45:27 +0000

    Delete exe.cjs

--- shared/exe.cjs
+++
@@ -1,8 +0,0 @@
-/** @todo after 2023-01-01
- * - Delete handle-deprecated-script-in-src.cjs
- * - Delete exe.cjs
- * - Use --require=suppress-experimental-warnings in package.json → scripts → exe
- */
-
-require("./exe/handle-deprecated-script-in-src.cjs");
-require("suppress-experimental-warnings");
