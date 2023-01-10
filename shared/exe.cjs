6638183d47505b69cdad0e3dd8d1b9b16793398a
Alexander Kachkaev <alexander@kachkaev.ru>
2023-01-10 22:44:08 +0000

    Update Luxon, fix runtime in the latest Node version

--- shared/exe.cjs
+++ shared/exe.cjs
@@ -1,9 +1,8 @@
-/** @todo after 2022-06-01
+/** @todo after 2023-01-01
  * - Delete handle-deprecated-script-in-src.cjs
  * - Delete exe.cjs
- * - Check if suppress-experimental-warnings.cjs is still needed
- * - Update package.json → scripts → exe
+ * - Use --require=suppress-experimental-warnings in package.json → scripts → exe
  */
 
 require("./exe/handle-deprecated-script-in-src.cjs");
-require("./exe/suppress-experimental-warnings.cjs");
+require("suppress-experimental-warnings");
